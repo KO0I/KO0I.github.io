e17bfe12a21aaf76ad97c50e86ec86509b18052c
Pat <paha5124@colorado.edu>
2026-06-05 16:27:06 -0600

    Attempt to add a dictionary

---
+++ assets/marain/js/sentences.js
@@ -0,0 +1,11 @@
+var sentences = [
+	{"marain":"ra'yuh prenva zawen",
+		"en-bow":"I on spaceship fly",
+		"en-id":"I am flying aboard a spaceship"},
+	{"marain":"ra'yuh kabo tingiye",
+		"en-bow":"I speak understanding with",
+		"en-id":"I know what I'm talking about"},
+	{"marain":"geyuh peraybihnt eseli",
+		"en-bow":"you seem to the culture beautiful",
+		"en-id":"you seem beautiful to the culture"}
+];
